9d2f8556ccf2c9ddbccb0e22345fb4176435a969
Nathaniel Kitzke <nat.kitzke@gmail.com>
2015-09-29 19:20:50 +0000

    Exercise-3 : Operations

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/op/:oper/:num1/:num2', function (req, res) {
+    var responseObject = doMath( req.params.oper, Number(req.params.num1), Number(req.params.num2) );
+    if(responseObject === "ERROR"){
+        res.sendStatus(400);
+    }else{
+        res.send( responseObject );
+    }
 });
 
-
-
+function doMath(operator, num1, num2){
+    switch (operator) {
+        case 'add':
+            return {
+              "operator": "add",
+              "firstOperand": num1,
+              "secondOperand": num2,
+              "solution": num1+num2
+            };
+            
+        case 'sub':
+            return {
+              "operator": "substract",
+              "firstOperand": num1,
+              "secondOperand": num2,
+              "solution": num1-num2
+            };
+            
+        case 'mult':
+            return {
+              "operator": "multiply",
+              "firstOperand": num1,
+              "secondOperand": num2,
+              "solution": num1*num2
+            };
+            
+        case 'div':
+            return {
+              "operator": "divide",
+              "firstOperand": num1,
+              "secondOperand": num2,
+              "solution": num1/num2
+            };
+        
+        default:
+            return "ERROR";
+    }
+}
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
