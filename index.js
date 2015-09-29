5d4a447f589956af7d1d62e6de24b471e3907fe1
Nathaniel Kitzke <nat.kitzke@gmail.com>
2015-09-29 18:40:46 +0000

    exercise-2 A wild param appeared

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/hello/:firstName', function (req, res) {
+    var name = req.params.firstName;
+    res.send('<h1>Hello '+ name +'!');
 });
 
-
+    
 
 
 
@@ -13,8 +14,8 @@ app.get('/', function (req, res) {
 
 // Boilerplate code to start up the web server
 var server = app.listen(process.env.PORT, process.env.IP, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+  
+    console.log('Example app listening at http://%s:%s', host, port);
 });
