6fe2693e61f9d52ceda13635a09fd763d07cf304
Nathaniel Kitzke <nat.kitzke@gmail.com>
2015-09-29 22:29:04 +0000

    Exercise-5 : Searching the data. Still can't search emails though, but I have tried a few things.

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var app = express();
+var entries = [
+    {
+        firstName: 'John',
+        lastName: "Smith",
+        emails: [
+            {type: 'home', address: 'john@smith.com'},
+            {type: "work", address: "jsmith@megacorp.com"}
+        ]
+    },
+    {
+        firstName: 'Kayla',
+        lastName: "Nadeau",
+        emails: [
+            {type: 'home', address: 'Kayla@sass.com'},
+            {type: "work", address: "Kayla@megacorp.com"}
+        ]
+    },
+    {
+        firstName: 'Martin',
+        lastName: "Greg",
+        emails: [
+            {type: 'home', address: 'Greg@fusion.com'},
+            {type: "work", address: "MartinGreg@megacorp.com"}
+        ]
+    },
+    {
+        firstName: 'Jules',
+        lastName: "Calipso",
+        emails: [
+            {type: 'home', address: 'Verne@fusion.com'},
+            {type: "work", address: "JulesVernes@megacorp.com"}
+        ]
+    }
+];
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/entry/:search', function (req, res) {
+    
+    var searchQuery = req.query;
+    console.log( entries[3].emails.forEach(function (el,index){ if(el.address.indexOf(searchQuery.lastName) >= 0) return true; } ) );
+    
+    console.log(   entries[3].emails.forEach(  function(el,index){ return el.address.indexOf(searchQuery.lastName) }  )   );
+    // var name1 = searchParams.slice( searchParams.indexOf("=") + 1, searchParams.indexOf("&") - 1 );
+    // var name2 = searchParams.slice( searchParams.indexOf( "=", searchParams.indexOf("&") ) + 1, searchParams.length);
+   
+    var entryMatches = entries.filter(function(ele,ind){
+        return ( searchQuery.firstName===ele.firstName || searchQuery.lastName===ele.lastName || searchQuery.firstName===ele.firstName || searchQuery.lastName===ele.lastName );
+    });
+    console.log(entryMatches);
+    res.send(entryMatches);
 });
+
+// || ele.emails.forEach(function (el,index){ return el.address.indexOf(searchQuery.firstName) }) >= 0 || ele.emails.forEach(function (el,index){ return el.address.indexOf(searchQuery.lastName) }) >= 0 );
+
 
 
 
